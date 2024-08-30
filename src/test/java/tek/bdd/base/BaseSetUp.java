48768eee21e227e7ff526d1f7fd583bf2d42efb4
nmasumi <nmasumi22@gmail.com>
2024-08-30 01:21:50 -0400

    setup complete

--- src/test/java/tek/bdd/base/BaseSetUp.java
+++ src/test/java/tek/bdd/base/BaseSetUp.java
@@ -1,20 +1,30 @@
 package tek.bdd.base;
 
-import io.cucumber.messages.types.Duration;
 import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class BaseSetUp {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     public void setupBrowser() {
         driver = new ChromeDriver();
         driver.get("https://dev.insurance.tekschool-students.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+
+    }
+    public void quitBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
-
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
