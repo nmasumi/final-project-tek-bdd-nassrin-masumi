1fd2dc8a69ea954c2f7b2dfdab8b09e7f7e18302
nmasumi <nmasumi22@gmail.com>
2024-08-30 03:01:39 -0400

    selenium utilities

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -1,6 +1,7 @@
 package tek.bdd.utility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetUp;
@@ -8,18 +9,21 @@ import tek.bdd.base.BaseSetUp;
 import java.time.Duration;
 
 public class SeleniumUtility extends BaseSetUp {
-
     private WebDriverWait getWait() {
         return new WebDriverWait(getDriver(), Duration.ofSeconds(20));
     }
-
+    private WebElement waitForVisibility(By locator) {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
     public void clickOnElement(By locator) {
         getWait().until(ExpectedConditions.elementToBeClickable(locator))
                 .click();
     }
-
     public void sendText(By locator, String value) {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator))
-                .sendKeys(value);
+        waitForVisibility(locator).sendKeys(value);
+    }
+    public String getElementText(By locator) {
+        return waitForVisibility(locator).getText();
     }
 }
+
